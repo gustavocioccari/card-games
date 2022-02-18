934df8e4c08b7f682fc3d14f856b75e4f0939373
Gustavo Cioccari <gustavocioccari@gmail.com>
2022-02-18 10:55:09 -0300

    feat: add DB connection config

---
+++ pkg/repositories/postgres/handler.go
@@ -0,0 +1,34 @@
+package postgres
+
+import (
+	"errors"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func GetDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=toggl port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, errors.New("error on connect to database")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetConnMaxLifetime(time.Minute * 5)
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(3)
+
+	err = sqlDB.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
